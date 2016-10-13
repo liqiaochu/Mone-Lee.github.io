ee1bdf11df168b1e3c327202d50171468e61ef15
Lee <1051933474@qq.com>
2016-10-13 17:35:25 +0800

    内容页

---
+++ js/page.js
@@ -0,0 +1,20 @@
+function boomIn(){
+	/**
+	 * 弹出联系方式
+	 */
+	var contactMe=document.getElementById("contactMe");
+	var overlay=document.getElementById("overlay");
+	var contactContent=document.getElementById("contactContent");
+	overlay.className="";		
+	contactContent.className="";	
+}
+function boomOut(){
+	/**
+	 * 关闭弹出联系方式
+	 */
+	var contactMe=document.getElementById("contactMe");
+	var overlay=document.getElementById("overlay");
+	var closeButton=document.getElementById("closeButton");
+	overlay.className="hide";
+	contactContent.className="hide";
+}
